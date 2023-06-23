4d898837f385bd9d89699ee7a61dd711ede5b03d
ghostyu2 <ghostyu2@gmail.com>
2023-06-23 19:59:25 -0300

    Ejercicio 1 resuelto

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,37 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let arrayNum = num.toString().split('');
+   console.log(arrayNum);
+   let i = arrayNum.length - 1;
+   let j = 0;
+   let numDecimal = 0;
+   while (i >= 0) {
+      numDecimal += parseInt(arrayNum[i],10)*2**j;
+      i -= 1;
+      j += 1;
+   }
 
-function DecimalABinario(num) {}
+   return numDecimal;
+
+}
+
+console.log(BinarioADecimal(10111))
+
+function DecimalABinario(num) {
+   let division = num;
+   let resto = 0;
+   let arrayBinario = [];
+   while (division > 0) {
+      resto  = division % 2;
+      arrayBinario.unshift(resto);
+      division = Math.floor(division / 2);
+   }
+   arrayBinario = arrayBinario.join('');
+   return arrayBinario;
+}
+
+console.log(DecimalABinario(29));
 
 module.exports = {
    BinarioADecimal,
